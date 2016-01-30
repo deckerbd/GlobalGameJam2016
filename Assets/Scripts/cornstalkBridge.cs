ea337a5236ceaeacdb624af1cf5c681212c32ac4
albertal <albertal@miamioh.edu>
2016-01-30 18:41:12 -0500

    cornstalk and dirtpile prefabs
    scripts n prefabs n shit

---
+++ Assets/Scripts/cornstalkBridge.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cornstalkBridge : MonoBehaviour 
+{
+	private Rigidbody2D rbCornstalk; 
+	private Animator anim; 
+	public float force; 
+	private bool isFalling = true; 
+	public bool fallRight;
+	public bool fallLeft;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rbCornstalk = GetComponent<Rigidbody2D> (); 
+		anim = GetComponent<Animator>(); 
+
+		if (this.gameObject.name == "cornstalkLargeRight(Clone)" || this.gameObject.name == "cornstalkLargeLeft(Clone)")
+		{
+			anim.SetBool ("isLarge", true); 
+		}
+
+		if (this.gameObject.name == "cornstalkMediumRight(Clone)" || this.gameObject.name == "cornstalkMediumLeft(Clone)") 
+		{
+			anim.SetBool ("isMedium", true); 
+		}
+
+		if (this.gameObject.name == "cornstalkSmallRight(Clone)" || this.gameObject.name == "cornstalkSmallLeft(Clone)") 
+		{
+			anim.SetBool ("isSmall", true); 
+		}
+	}
+
+	//collision
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "pit")
+		{
+			rbCornstalk.isKinematic = true; 
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(isFalling == true)
+		{
+			Invoke ("falling", .5f);
+		}
+	}
+
+	void falling()
+	{
+		isFalling = false;
+		if (fallRight == true) 
+		{
+			rbCornstalk.AddForce (new Vector2 (.5f, -.5f) * force);
+		}
+		if (fallLeft == true)
+		{
+			rbCornstalk.AddForce (new Vector2 (-.5f, -.5f) * force);
+		}
+	}
+}
---
+++ Assets/Scripts/moundScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moundScript : MonoBehaviour 
+{
+	public GameObject cornstalk;
+
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player" && Input.GetKeyDown ("f"))
+		{
+			Instantiate (cornstalk, transform.position, Quaternion.identity);
+		}
+			
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
