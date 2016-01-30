73db13cf94ce64b3f2d9867a48ecb5fa65e3b535
albertal <albertal@miamioh.edu>
2016-01-30 16:02:15 -0500

    Cornstalkanim
    Cornstalk animation and mouse stab corn fix. Still need to finish
    scripting on cornstalk falling.

---
+++ Assets/cornstalkBridge.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cornstalkBridge : MonoBehaviour 
+{
+	private Rigidbody2D rbCornstalk; 
+	private Animator anim; 
+	private bool isFalling = false; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		rbCornstalk = GetComponent<Rigidbody2D> (); 
+		anim = GetComponent<Animator>(); 
+
+		if (this.gameObject.name == "cornstalkLarge")
+		{
+			anim.SetBool ("isLarge", true); 
+		}
+
+		if (this.gameObject.name == "cornstalkMedium") 
+		{
+			anim.SetBool ("isMedium", true); 
+		}
+
+		if (this.gameObject.name == "cornstalkSmall") 
+		{
+			anim.SetBool ("isSmall", true); 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (isFalling == true) 
+		{
+			
+		}
+	}
+}
