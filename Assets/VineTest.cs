c2da7f169bf17f09abf02165229931df597d03ec
Kalseng <mqholm@gmail.com>
2016-01-30 16:46:27 -0500

    VineWork

---
+++ Assets/VineTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VineTest : MonoBehaviour {
+
+	public GameObject bgWall;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
