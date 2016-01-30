6a551595d2964b44d89f58c998a9872b5cd7c8e9
Ben Decker <deckerbd@miamioh.edu>
2016-01-30 04:14:15 -0500

    Added Camera Control and WitchMegaJump (unlimited jumping)
    Added Camera Control and WitchMegaJump (unlimited jumping)

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public Transform farLeft;
+    public Transform farRight;
+    public Transform farTop;
+    public Transform farBottom;
+    private const float X_TOLERANCE = 2f;
+    private const float Y_TOLERANCE = 5.5f;
+    private const float CHARACTER_Y_OFFSET = 4.2f;
+
+    void Start()
+    {
+        farLeft = GameObject.Find("LeftBound").transform;
+        farRight = GameObject.Find("RightBound").transform;
+        farTop = GameObject.Find("UpperBound").transform;
+        farBottom = GameObject.Find("LowerBound").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 newPosition = this.transform.position;
+
+        // if player to the right of the camera
+        if (player.transform.position.x - this.transform.position.x > X_TOLERANCE)
+        {
+            newPosition.x = Mathf.Lerp(this.transform.position.x, player.transform.position.x - (X_TOLERANCE + 0.1f), 0.5f);
+        }
+        // if player to the left of the camera
+        else if (player.transform.position.x - this.transform.position.x < -1f*X_TOLERANCE)
+        {
+            newPosition.x = Mathf.Lerp(this.transform.position.x, player.transform.position.x + (X_TOLERANCE + 0.1f), 0.5f);
+        }
+
+        // if player goes above the camera
+        if ((player.transform.position.y + CHARACTER_Y_OFFSET) - this.transform.position.y > Y_TOLERANCE)
+        {
+            newPosition.y = Mathf.Lerp(this.transform.position.y, (player.transform.position.y + CHARACTER_Y_OFFSET) - (Y_TOLERANCE + 0.1f), 0.5f);
+        }
+        // if player goes below the camera
+        else if ((player.transform.position.y+CHARACTER_Y_OFFSET ) - this.transform.position.y < 0f)
+        {
+            newPosition.y = Mathf.Lerp(this.transform.position.y, (player.transform.position.y + CHARACTER_Y_OFFSET) + 0.1f, 0.5f);
+        }
+
+
+        //newPosition.x = Mathf.Clamp(newPosition.x, farLeft.position.x, farRight.position.x);
+        newPosition.x = Mathf.Clamp(newPosition.x, farLeft.position.x, farRight.position.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, farBottom.position.y, farTop.position.y);
+        this.transform.position = newPosition;
+    }
+
+}
---
+++ Assets/Scripts/WitchMegaJumpController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WitchMegaJumpController : MonoBehaviour {
+
+    public float movementModifier = 2.5f;
+
+    public GameObject groundCheck;
+    public float jumpModifier = 200f;
+
+    public bool grounded = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputManager();
+        JumpManager();
+    }
+
+    void InputManager()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * movementModifier, this.gameObject.GetComponent<Rigidbody2D>().velocity.y);
+            if (this.gameObject.GetComponent<Rigidbody2D>().velocity.x > 0)
+            {
+                if (this.gameObject.transform.localScale.x < 0)
+                {
+                    this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x * -1, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
+                }
+            }
+            else if (this.gameObject.GetComponent<Rigidbody2D>().velocity.x < 0)
+            {
+                if (this.gameObject.transform.localScale.x > 0)
+                {
+                    this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x * -1, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
+                }
+            }
+        }
+    }
+
+    void JumpManager()
+    {
+        JumpChecker();
+        if (grounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+                this.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpModifier);
+            }
+        }
+    }
+
+    void JumpChecker()
+    {
+        //grounded = Physics2D.Linecast(transform.position, groundCheck.transform.position, 1 << LayerMask.NameToLayer("Walkable"));
+        grounded = true;
+    }
+}
