bb39f9b4c17b758766b650bc760c1ffa6422250b
Kalseng <mqholm@gmail.com>
2016-01-31 15:35:47 -0500

    FinalScene

--- Assets/EndingScrip.cs
+++ Assets/EndingScrip.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class EndingScrip : MonoBehaviour {
@@ -9,7 +10,7 @@ public class EndingScrip : MonoBehaviour {
 
 	public GameObject[] cameraLocations;
 
-	public float fadeTime = 50.0f;
+	public float fadeTime = 49.0f;
 
 	private float counter;
 
@@ -17,16 +18,23 @@ public class EndingScrip : MonoBehaviour {
 	public Color current;
 	public Color fadeFrom;
 
+	public AudioSource crowd;
+
+	public AudioClip rowdy, calm;
+
 	// Use this for initialization
 	void Start () {
-		current.a = Mathf.Lerp (fadeFrom.a, fadeTo.a, counter / fadeTime);
-		MorePopulatedSprite.GetComponent<SpriteRenderer> ().color = current;
+		StartCoroutine ("PlayScript");
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		counter += Time.deltaTime;
+		current.a = Mathf.Lerp (fadeFrom.a, fadeTo.a, counter / fadeTime);
+		MorePopulatedSprite.GetComponent<SpriteRenderer> ().color = current;
+
+		crowd.volume = counter / fadeTime;
 
 	}
 
@@ -40,5 +48,11 @@ public class EndingScrip : MonoBehaviour {
 			yield return new WaitForSeconds (this.GetComponent<AudioSource> ().clip.length);
 			linecount++;
 		}
+
+		crowd.clip = rowdy;
+		crowd.Play ();
+
+		yield return new WaitForSeconds (5.0f);
+		SceneManager.LoadScene ("Credits");
 	}
 }
